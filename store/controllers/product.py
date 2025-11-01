6842295cb032e510ff281926d2c0d8599ea3c90b
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2025-11-01 20:35:02 -0300

    Implement error handling for product operations
    Added error handling for product creation and update operations, including specific exceptions for MongoDB errors and general exceptions.

--- store/controllers/product.py
+++ store/controllers/product.py
@@ -1,8 +1,10 @@
-from typing import List
-from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
+from typing import List, Optional
+from decimal import Decimal
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, status, Query
 from pydantic import UUID4
-from store.core.exceptions import NotFoundException
+from pymongo.errors import PyMongoError
 
+from store.core.exceptions import NotFoundException
 from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
 from store.usecases.product import ProductUsecase
 
@@ -13,7 +15,17 @@ router = APIRouter(tags=["products"])
 async def post(
     body: ProductIn = Body(...), usecase: ProductUsecase = Depends()
 ) -> ProductOut:
-    return await usecase.create(body=body)
+    try:
+        return await usecase.create(body=body)
+    except PyMongoError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occurred while inserting the product: {exc}",
+        )
+    except Exception as exc:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exc)
+        )
 
 
 @router.get(path="/{id}", status_code=status.HTTP_200_OK)
@@ -27,8 +39,12 @@ async def get(
 
 
 @router.get(path="/", status_code=status.HTTP_200_OK)
-async def query(usecase: ProductUsecase = Depends()) -> List[ProductOut]:
-    return await usecase.query()
+async def query(
+    usecase: ProductUsecase = Depends(),
+    price_min: Optional[Decimal] = Query(None, alias="price_min"),
+    price_max: Optional[Decimal] = Query(None, alias="price_max"),
+) -> List[ProductOut]:
+    return await usecase.query(price_min=price_min, price_max=price_max)
 
 
 @router.patch(path="/{id}", status_code=status.HTTP_200_OK)
@@ -37,7 +53,10 @@ async def patch(
     body: ProductUpdate = Body(...),
     usecase: ProductUsecase = Depends(),
 ) -> ProductUpdateOut:
-    return await usecase.update(id=id, body=body)
+    try:
+        return await usecase.update(id=id, body=body)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
 
 
 @router.delete(path="/{id}", status_code=status.HTTP_204_NO_CONTENT)
