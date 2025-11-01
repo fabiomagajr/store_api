f84d869b30e6d96762daf3cc610605d7ade8b4f0
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2025-11-01 20:35:28 -0300

    Enhance product use case with error handling and filtering
    Added error handling for product creation and updated query method to filter by price range.

--- store/usecases/product.py
+++ store/usecases/product.py
@@ -1,7 +1,10 @@
-from typing import List
+from typing import List, Optional
 from uuid import UUID
+from datetime import datetime
+from decimal import Decimal
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 import pymongo
+from pymongo.errors import PyMongoError
 from store.db.mongo import db_client
 from store.models.product import ProductModel
 from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
@@ -15,10 +18,13 @@ class ProductUsecase:
         self.collection = self.database.get_collection("products")
 
     async def create(self, body: ProductIn) -> ProductOut:
-        product_model = ProductModel(**body.model_dump())
-        await self.collection.insert_one(product_model.model_dump())
+        try:
+            product_model = ProductModel(**body.model_dump())
+            await self.collection.insert_one(product_model.model_dump())
 
-        return ProductOut(**product_model.model_dump())
+            return ProductOut(**product_model.model_dump())
+        except PyMongoError as exc:
+            raise exc
 
     async def get(self, id: UUID) -> ProductOut:
         result = await self.collection.find_one({"id": id})
@@ -28,13 +34,29 @@ class ProductUsecase:
 
         return ProductOut(**result)
 
-    async def query(self) -> List[ProductOut]:
-        return [ProductOut(**item) async for item in self.collection.find()]
+    async def query(
+        self,
+        price_min: Optional[Decimal] = None,
+        price_max: Optional[Decimal] = None,
+    ) -> List[ProductOut]:
+        filter_query = {}
+        if price_min is not None and price_max is not None:
+            filter_query["price"] = {
+                "$gt": float(price_min),
+                "$lt": float(price_max),
+            }
+
+        return [ProductOut(**item) async for item in self.collection.find(filter_query)]
 
     async def update(self, id: UUID, body: ProductUpdate) -> ProductUpdateOut:
+        product = await self.get(id=id)
+
+        body.updated_at = datetime.now()
+        update_data = body.model_dump(exclude_none=True)
+
         result = await self.collection.find_one_and_update(
             filter={"id": id},
-            update={"$set": body.model_dump(exclude_none=True)},
+            update={"$set": update_data},
             return_document=pymongo.ReturnDocument.AFTER,
         )
 
