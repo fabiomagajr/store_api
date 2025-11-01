eb63098ef3686da9f7e30d494106dd8a388c32a7
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2025-11-01 20:35:50 -0300

    Refactor Product schema to use BaseModel

--- store/schemas/product.py
+++ store/schemas/product.py
@@ -1,37 +1,32 @@
+from datetime import datetime
 from decimal import Decimal
-from typing import Annotated, Optional
-from bson import Decimal128
-from pydantic import AfterValidator, Field
-from store.schemas.base import BaseSchemaMixin, OutSchema
+from typing import Optional
+from pydantic import BaseModel, Field
+from store.models.base import CreateBaseModel
 
 
-class ProductBase(BaseSchemaMixin):
+class ProductBase(BaseModel):
     name: str = Field(..., description="Product name")
     quantity: int = Field(..., description="Product quantity")
     price: Decimal = Field(..., description="Product price")
-    status: bool = Field(..., description="Product status")
+    status: bool = Field(..., description="Product status (active/inactive)")
 
 
-class ProductIn(ProductBase, BaseSchemaMixin):
-    ...
+class ProductIn(ProductBase):
+    pass
 
 
-class ProductOut(ProductIn, OutSchema):
-    ...
+class ProductOut(ProductIn, CreateBaseModel):
+    pass
 
 
-def convert_decimal_128(v):
-    return Decimal128(str(v))
-
-
-Decimal_ = Annotated[Decimal, AfterValidator(convert_decimal_128)]
-
-
-class ProductUpdate(BaseSchemaMixin):
+class ProductUpdate(BaseModel):
+    name: Optional[str] = Field(None, description="Product name")
     quantity: Optional[int] = Field(None, description="Product quantity")
-    price: Optional[Decimal_] = Field(None, description="Product price")
+    price: Optional[Decimal] = Field(None, description="Product price")
     status: Optional[bool] = Field(None, description="Product status")
+    updated_at: Optional[datetime] = Field(None, description="Timestamp of last update")
 
 
 class ProductUpdateOut(ProductOut):
-    ...
+    pass
